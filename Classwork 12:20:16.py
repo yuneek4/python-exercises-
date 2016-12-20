69b9980985de44fc381bef22552b7f8b2b01b6dc
yuneek4 <yuneek4@gmail.com>
2016-12-20 14:24:31 -0500

    Add files via upload

---
+++ Classwork 12:20:16.py
@@ -0,0 +1,25 @@
+# Excersise 1
+
+year1=100*1.1
+print(year1)
+
+year7=year1*1.1**7
+print(year7)
+
+# Excercise 2
+
+saving=100
+factor=1.10
+print(saving*factor**8)
+
+# Excercise 3
+
+desc="compound interest"
+profitable= True
+
+# Excercise 4
+print(saving,factor,desc,profitable)
+
+# Excercise 5
+
+print('I started with $100 and now I have $214. Great!') 
